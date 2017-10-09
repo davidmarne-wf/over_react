f0fa09733e3d837f2233a0aebab5324ea95b6a8f
davidmarne-wf <david.marne@workiva.com>
2017-10-09 12:03:23 -0600

    connect proposal

--- lib/src/component_declaration/redux_component.dart
+++ lib/src/component_declaration/redux_component.dart
@@ -17,13 +17,15 @@ library over_react.component_declaration.redux_component;
 import 'dart:async';
 
 import 'package:built_redux/built_redux.dart';
+import 'package:built_value/built_value.dart';
 import 'package:meta/meta.dart';
 import 'package:over_react/over_react.dart';
 
 /// Builds on top of [UiProps], adding typed props for [Store] in order to integrate with `built_redux`.
 ///
 /// Use with the over_react transformer via the `@Props()` ([annotations.Props]) annotation.
-abstract class ReduxUiProps<StoreT> extends UiProps {
+abstract class ReduxUiProps<V extends Built<V, B>, B extends Builder<V, B>,
+    A extends ReduxActions> extends UiProps {
   String get _storePropKey => '${propKeyNamespace}store';
 
   /// The prop defined by [StoresT].
@@ -33,8 +35,10 @@ abstract class ReduxUiProps<StoreT> extends UiProps {
   /// __Instead of storing state within this component via `setState`, it is recommended that data be
   /// pulled directly from these stores.__ This ensures that the data being used is always up to date
   /// and leaves the state management logic to the stores.
-  StoreT get store => props[_storePropKey];
-  set store(StoreT value) => props[_storePropKey] = value;
+  Store<V, B, A> get store => props[_storePropKey];
+  set store(Store<V, B, A> value) => props[_storePropKey] = value;
+
+  A get actions => store.actions;
 }
 
 /// Builds on top of [UiComponent], adding `built_redux` integration.
@@ -47,30 +51,14 @@ abstract class ReduxUiProps<StoreT> extends UiProps {
 /// Use with the over_react transformer via the `@Component()` ([annotations.Component]) annotation.
 ///
 /// > Related: [ReduxUiStatefulComponent]
-abstract class ReduxUiComponent<T extends ReduxUiProps> extends UiComponent<T> with _ReduxComponentMixin<T> {
+abstract class ReduxUiComponent<V extends Built<V, B>, B extends Builder<V, B>,
+        A extends ReduxActions, T extends ReduxUiProps<V, B, A>, Substate>
+    extends UiComponent<T> with _ReduxComponentMixin<V, B, A, Substate> {
   @mustCallSuper
   @override
   void componentWillMount() {
     super.componentWillMount();
     _setUpSubs();
-  }
-
-  @mustCallSuper
-  @override
-  void componentWillReceiveProps(Map nextProps) {
-    super.componentWillReceiveProps(nextProps);
-    // Unbind store subs so they can be re-bound in componentDidUpdate
-    // once the new props are available, ensuring the values returned by [redrawOn]
-    // are not outdated.
-    _tearDownSubs();
-  }
-
-  @mustCallSuper
-  @override
-  void componentDidUpdate(Map prevProps, Map prevState) {
-    // If the subs are not bound at this point, then that means they were unbound by
-    // componentWillReceiveProps, and need to be re-bound now that new props are available.
-    if (!_areStoreSubsBound) _setUpSubs();
   }
 
   @mustCallSuper
@@ -91,31 +79,19 @@ abstract class ReduxUiComponent<T extends ReduxUiProps> extends UiComponent<T> w
 /// Use with the over_react transformer via the `@Component()` ([annotations.Component]) annotation.
 ///
 /// > Related: [ReduxUiComponent]
-abstract class ReduxUiStatefulComponent<T extends ReduxUiProps, S extends UiState> extends UiStatefulComponent<T, S>
-    with _ReduxComponentMixin<T> {
+abstract class ReduxUiStatefulComponent<
+        V extends Built<V, B>,
+        B extends Builder<V, B>,
+        A extends ReduxActions,
+        S extends UiState,
+        T extends ReduxUiProps<V, B, A>,
+        Substate> extends UiStatefulComponent<T, S>
+    with _ReduxComponentMixin<V, B, A, Substate> {
   @mustCallSuper
   @override
   void componentWillMount() {
     super.componentWillMount();
     _setUpSubs();
-  }
-
-  @mustCallSuper
-  @override
-  void componentWillReceiveProps(Map nextProps) {
-    super.componentWillReceiveProps(nextProps);
-    // Unbind store subs so they can be re-bound in componentDidUpdate
-    // once the new props are available, ensuring the values returned by [redrawOn]
-    // are not outdated.
-    _tearDownSubs();
-  }
-
-  @mustCallSuper
-  @override
-  void componentDidUpdate(Map prevProps, Map prevState) {
-    // If the subs are not bound at this point, then that means they were unbound by
-    // componentWillReceiveProps, and need to be re-bound now that new props are available.
-    if (!_areStoreSubsBound) _setUpSubs();
   }
 
   @mustCallSuper
@@ -126,38 +102,30 @@ abstract class ReduxUiStatefulComponent<T extends ReduxUiProps, S extends UiStat
   }
 }
 
-abstract class _ReduxComponentMixin<T extends ReduxUiProps> implements UiComponent<T> {
-  List<Stream> redrawOn() {
-    if (props.store is Store) {
-      return <Stream>[props.store.stream];
-    } else {
-      return <Stream>[];
-    }
-  }
+abstract class _ReduxComponentMixin<
+    V extends Built<V, B>,
+    B extends Builder<V, B>,
+    A extends ReduxActions,
+    Substate> implements UiComponent<ReduxUiProps<V, B, A>> {
+  Substate _reduxState;
+  Substate get reduxState => _reduxState;
 
-  bool get _areStoreSubsBound => _storeSubs != null;
+  // implemented by the concrete component to subscribe to changes
+  // to the values from the redux store that this component cares about
+  Substate connect(V state);
 
-  List<StreamSubscription> _storeSubs;
+  StreamSubscription _storeSub;
 
   void _setUpSubs() {
-    if (_areStoreSubsBound) {
-      throw new StateError('Store subscriptions are already bound.');
-    }
-
-    _storeSubs = [];
-
-    for (var stream in redrawOn()) {
-      _storeSubs.add(stream.listen((_) => redraw()));
-    }
+    // when the values we care about change cache off the new value and
+    // redraw
+    _storeSub = props.store.nextSubstate(connect).listen((Substate s) {
+      _reduxState = s;
+      redraw();
+    });
   }
 
   void _tearDownSubs() {
-    if (!_areStoreSubsBound) return;
-
-    for (var sub in _storeSubs) {
-      sub.cancel();
-    }
-
-    _storeSubs = null;
+    _storeSub.cancel();
   }
 }
